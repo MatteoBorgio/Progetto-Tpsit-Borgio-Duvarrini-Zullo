b63dc594dee34af00a1efcf057efa7f3a20e7e2a
matteoborgio <matteo6.borgio@gmail.com>
2026-04-14 18:54:03 +0200

    add basic configuration in server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = 5000;
+
+const clientRoutes = require('./routes/clientRoutes');
+const exportRoutes = require('./routes/exportRoutes');
+const invoiceRoutes = require('./routes/invoiceRoutes');
+
+const CLIENTS_DB = './data/clients.json';
+const INVOICES_DB = './data/invoices.json';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/clients', clientRoutes),
+    app.use('/export', exportRoutes);
+app.use('/invoices', invoiceRoutes);
+
+app.use((req, res) => {
+    res.send(`<h1>Errore 404 - Pagina non trovata</h1>`);
+});
+
+app.listen(PORT, () => {
+    console.log("Server attivo su localhost:" + PORT);
+});
