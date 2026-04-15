1d4be022da69e63b5169b42e5ba213ddb1606719
matteoborgio <matteo6.borgio@gmail.com>
2026-04-15 20:45:09 +0200

    add get /clients/:id in clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -36,3 +36,58 @@ router.get('/', (req, res) => {
         })
     }
 });
+
+
+/**
+ * Rotta get /clients/:id
+ * Recupera i dati di un cliente specifico attraverso un id univoco inserito 
+ * nei parametri della richiesta
+ */
+router.get('/:id', (req, res) => {
+    try {
+        // Verifichiamo l'esistenza del file clients.json
+        if (!fs.existsSync(CLIENT_DB)) {
+            return res.status(404).json({
+                success: false,
+                message: "Si è verificato un errore.",
+                errore: "File non trovato."
+            });
+        }
+
+        // Recuperiamo l'id dai parametri della richiesta 
+        // e verifichiamo sia un numero
+        const id = parseInt(req.params.id);
+        if (!id || isNan(id)) {
+            return res.status(401).json({
+                success: false,
+                message: "Si è verificato un errore.",
+                error: "Id non inserito correttamente"
+            });
+        }
+
+        const data = fs.readFileSync(CLIENT_DB, 'utf-8');
+        const clients = JSON.parse(data);
+
+        // Recuperiamo i dati di un cliente specifico e li restituiamo al frontend 
+        const client = clients.find((c) => parseInt(c.id) === id);
+        if (!client) {
+            return res.status(404).json({
+                success: false,
+                message: "Si è verificato un errore.",
+                error: "Cliente non trovato"
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Dati del cliente recuperati.",
+            results: client
+        });
+    } catch (error) {
+        console.log("Si è verificato un errore: " + error);
+        return res.status(500).json({
+            success: false,
+            message: "Si è verificato un errore.",
+            error: error
+        })
+    }
+});
