ca04eb4798934e18a1df240019ab310ef2795bee
shishalex <alx8.ps4@gmail.com>
2026-04-20 08:50:57 +0200

    add get by id in routes/invoiceRoutes.js

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -33,3 +33,43 @@ router.get("/", (req, res) => {
         return sendError(res, 500, "Errore interno del server.");
     }
 });
+
+/**
+ * Rotta get /invoices/:id
+ * Recupera i dati di una fattura specifica attraverso un id univoco inserito
+ * nei parametri della richiesta
+ */
+router.get("/:id", (req, res) => {
+    try{
+        // Verifichiamo l'esistenza del file invoices.json
+        if (!fs.existsSync(INVOICES_DB)) {
+            return sendError(res, 404, "File non trovato");
+        }
+
+        // Recuperiamo l'id dai parametri della richiesta
+        // e verifichiamo sia un numero
+        const id = parseInt(req.params.id);
+        if (!id || isNaN(id)) {
+            return sendError(res, 400, "Id non inserito correttamente");
+        }
+
+        const data = fs.readFileSync(INVOICES_DB, "utf-8");
+        const invoices = JSON.parse(data);
+
+        // Recuperiamo i dati di una fattura specifica e li restituiamo al frontend
+        const invoice = invoices.find((i) => parseInt(i.id) === id);
+        if (invoice) {
+            return sendError(res, 404, "Fattura non trovata.");
+        }
+        return sendSuccessResponse(
+            res,
+            200,
+            "Dati della fattura recuperati.",
+            invoice,
+        );
+    } catch (error) {
+        console.log("SI è verificato un errore: " + error);
+        return sendError(res, 500, "Errore interno del server.");
+    }
+
+})
